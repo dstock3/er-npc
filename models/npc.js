cb410059e4fa734171a71558e04565e87dfb576c
David Stockdale <dstock3@gmail.com>
2022-04-21 13:13:51 -0500

    Created model for npc.

---
+++ models/npc.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var npcSchema = new Schema(
+  {
+    name: {type: String, required: true, maxLength: 100},
+    desc: {type: String, required: true},
+    category: {type: Schema.Types.ObjectId, ref: 'Category', required: true},
+    loc: {type: String, required: true, maxLength: 100},
+    quote: {type: String, required: false, maxLength: 280},
+    notes: {type: String, required: false, maxLength: 280}
+  }
+);
+
+// Virtual for npc's URL
+npcSchema
+.virtual('url')
+.get(function () {
+  return '/npc/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('npc', npcSchema);
