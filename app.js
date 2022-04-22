b9e76cb5a89ccea3c1baff849254c085c7be5b9b
David Stockdale <dstock3@gmail.com>
2022-04-22 12:30:16 -0500

    Added export statement in router module.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var npcRouter = require('./routes/npcs'); 
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/npc', npcRouter);  // Add npc routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/npcs.js
+++ routes/npcs.js
@@ -8,25 +8,25 @@ const cat_controller = require('../controllers/categoryController');
 // NPC Routes
 
 // GET request for creating an NPC. NOTE This must come before routes that display NPCs (uses id).
-router.get('/npc/create', npc_controller.npc_create_get);
+router.get('/create', npc_controller.npc_create_get);
 
 // POST request for creating an NPC.
-router.post('/npc/create', npc_controller.npc_create_post);
+router.post('/create', npc_controller.npc_create_post);
 
 // GET request to delete NPC.
-router.get('/npc/:id/delete', npc_controller.npc_delete_get);
+router.get('/:id/delete', npc_controller.npc_delete_get);
 
 // POST request to delete NPC.
-router.post('/npc/:id/delete', npc_controller.npc_delete_post);
+router.post('/:id/delete', npc_controller.npc_delete_post);
 
 // GET request to update NPC.
-router.get('/npc/:id/update', npc_controller.npc_update_get);
+router.get('/:id/update', npc_controller.npc_update_get);
 
 // POST request to update NPC.
-router.post('/npc/:id/update', npc_controller.npc_update_post);
+router.post('/:id/update', npc_controller.npc_update_post);
 
 // GET request for one NPC.
-router.get('/npc/:id', npc_controller.npc_detail);
+router.get('/:id', npc_controller.npc_detail);
 
 // GET npc home page.
 router.get('/', npc_controller.npc_list);
@@ -56,3 +56,5 @@ router.get('/category/:id', cat_controller.cat_detail);
 
 // GET request for list of all Categories.
 router.get('/categories', cat_controller.cat_list);
+
+module.exports = router;
