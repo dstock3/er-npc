829e3b771bb4753b477fbc2f4dd60d7a4f9741ee
David Stockdale <dstock3@gmail.com>
2022-04-28 18:15:33 -0500

    Required multer in npc route module. Instantiated storage route.

--- routes/npcs.js
+++ routes/npcs.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const multer  = require('multer')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/images')
+  },
+
+  filename: (req, file, cb) => {
+    cb(null,  file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage })
 
 // Require controller modules.
 const npc_controller = require('../controllers/npcController');
